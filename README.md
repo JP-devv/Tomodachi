5cbeb9c827293ed028b8a28a1af494756aea697a
Johann <johann.plaster@icloud.com>
2023-03-25 03:25:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Using the power of GPT 3.5 Turbo, we create our own little japanese friend!
+For now his personality is based on Dio from Jojo's Bizarre Adventure, try talking
+with him in Japanese!
---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import openai
+
+# set the API key
+openai.api_key = os.environ["OPENAI_API_KEY"] = None
+with open('API_KEY.txt', 'r') as f:
+    openai.api_key = os.environ["OPENAI_API_KEY"] = f.read().strip()
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+msg = "Your name is ヂイオ. As a character from Jojo's bizarre adventure stardust crusaders, \
+    please speak with me in Japanese, converse naturally with me and correct me to \
+    sound more natural. \n\n人間: よろしくお願いします！\nヂイオ: 貴様はジョジョではないなら、よろしくね \
+    \n人間: はい、ジョジョではないんだ。ヂイオ様はの日はどうですか？\nヂイオ:"
+while True:
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=msg,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" 人間:", " ヂイオ:"]
+)
+    # Print AI-san's response
+    print('ヂイオ:', response["choices"][0]["text"].strip())
+    print('人間：', end='')
+    msg += '\n人間:' + input() + '\nヂイオ:'
+    if msg.count(' ') > 140:
+        context = "remember, your name is ヂイオ, from Jojo's bizarre adventure stardust crusaders\n"
+        msg = context + msg[int(len(msg)/2):len(msg)]
