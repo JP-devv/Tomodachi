7316c0b313714a54a299cb7127dba37f493bc2a2
Johann <johann.plaster@icloud.com>
2023-03-25 04:02:07 -0500

    Fixed an issue where the chatbot repeats its tag twice

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ with open('API.key', 'r') as f:
     key = f.read().strip()
 openai.api_key = os.environ["OPENAI_API_KEY"] = key
 
-start_sequence = "\nヂイオ:"
-restart_sequence = "\n人間:"
-
-msg = "Your name is ヂイオ. As a character from Jojo's bizarre adventure stardust crusaders, \
-    please speak with me in Japanese, converse naturally with me and correct me to \
-    sound more natural. \n\n人間: よろしくお願いします！\nヂイオ: 貴様はジョジョではないなら、よろしくね \
-    \n人間: はい、ジョジョではないんだ。ヂイオ様はの日はどうですか？\nヂイオ:"
+msg = "Context: You will play a character called ヂイオ. Being from the anime \
+    Jojo's bizarre adventure stardust crusaders, please speak with me in Japanese, \
+    converse naturally with me and correct me to sound more natural.\
+    \n人間: よろしくお願いします！\
+    \nヂイオ: 貴様はジョジョではないなら、よろしくね \
+    \n人間: はい、ジョジョではないんだ。ヂイオ様はの日はどうですか？ \
+    \nヂイオ:"
 while True:
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=msg,
     temperature=0.9,
-    max_tokens=150,
+    max_tokens=256,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,
-    stop=[" 人間:", " ヂイオ:"]
+    stop=["人間:", " ヂイオ:"]
 )
     # Print AI-san's response
-    print('ヂイオ:', response["choices"][0]["text"].strip())
+    feedback = response["choices"][0]["text"].strip()
+    print('ヂイオ:', feedback)
     print('人間：', end='')
-    msg += '\n人間:' + input() + '\nヂイオ:'
-    if msg.count(' ') > 140:
-        context = "remember, your name is ヂイオ, from Jojo's bizarre adventure stardust crusaders\n"
+    
+    # By appending msg, the chatbot somewhat remembers past items
+    msg += feedback + '\n人間: ' + input() + '\nヂイオ:'
+    
+    # If we are almost exceeding the word count, cut it in half
+    if msg.count(' ') > 240:
+        context = "Stay in character! You're ヂイオ from \
+            Jojo's bizarre adventure stardust crusaders\n"
         msg = context + msg[int(len(msg)/2):len(msg)]
