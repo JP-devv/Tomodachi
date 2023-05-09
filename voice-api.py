7de6f433cdf25922c3fc1f142983d8e3d3723f13
Johann <johann.plaster@icloud.com>
2023-05-08 21:38:59 -0500

    updated file

--- voice-api.py
+++ voice-api.py
@@ -1,27 +1,8 @@
-#6 strong woman
-#10 small girl
-#11 nasal guy
-#14 book girl
-#15-18 eroi onna
-#19 whispering girl
-#28 obachan
-#35 old lady 2
-#27 light whisper girl
-#39-40 npc guy
-#42 sakeji
-#49 super shy girl
-#50 whisper shy girl
-#51 butler 
-#53 npc guy 3
-#54 betty
-#55 boy
-
 #21 narrator guy
 #8 news woman
 #17 ara ara
 #13 deep voice man
 
-# paste this
 import sys
 import asyncio
 import io
